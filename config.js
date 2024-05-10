5226390f54d81fd07a617918d0013ff299dfbc29
Naveed511 <161928465+Naveed511@users.noreply.github.com>
2024-05-10 10:33:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ if (fs.existsSync('config.env')) {
 
 const API = "https://api-aswin-sparky.koyeb.app";
 const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:14be06d8f8fbd3506036a6749908a79f";
+const SUDO = process.env.SUDO || '03130510486';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
 const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "true";
 const BOT_INFO = process.env.BOT_INFO || "X-BOT-MD;ASWIN SPARKY;https://i.imgur.com/r3GZeiX.jpeg";
